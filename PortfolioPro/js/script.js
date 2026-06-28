e1fd7b7432be6a592d82633415191e556b29b05c
Dushyanth Rajan <dushyanth2007.info@gmail.com>
2026-06-28 17:34:32 +0530

    Added login page

--- PortfolioPro/js/script.js
+++ PortfolioPro/js/script.js
@@ -1,1 +1,5 @@
-console.log("PortfolioPro Started...");
+console.log("🚀 PortfolioPro v1.1 Loaded Successfully!");
+
+document.querySelector(".primary").addEventListener("click", () => {
+    alert("Welcome to PortfolioPro by DR SoftLabs!");
+});
